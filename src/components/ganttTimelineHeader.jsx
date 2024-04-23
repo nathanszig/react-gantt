2e9e69f1bdd460d168cb4e496cc44114040ea1b4
SunnyBenji <benjamin.burstein@edu.devinci.fr>
2024-04-23 12:07:44 +0200

    [RG-26] - add yearContainer
    group weeks by week in the dom

--- src/components/ganttTimelineHeader.jsx
+++ src/components/ganttTimelineHeader.jsx
@@ -1,64 +1,74 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import moment from "moment";
 import { weekHaveTask, getWeekList } from '../assets/utils/ganttUtils';
 
 const GanttTimelineHeader = (props) => {
-
   const timelineWeeks = getWeekList(props.users);
+  const yearsContainerRef = useRef(null);
 
-    return (
-        <div className="gantt-container-section-timeline-header">
-            {timelineWeeks.map((week, index) => {
+  // Créer un objet où chaque clé est une année et la valeur est un tableau de semaines correspondant à cette année
+  const weeksByYear = timelineWeeks.reduce((acc, week) => {
+    const year = moment(week.end, "YYYY-MM-DD").year();
+    if (!acc[year]) {
+      acc[year] = [];
+    }
+    acc[year].push(week);
+    return acc;
+  }, {});
+
+
+  return (
+    <div className="gantt-container-section-timeline-header">
+      {Object.keys(weeksByYear).map(year => (
+        <div key={year} className="gantt-container-section-timeline-header-weeks">
+          <div className="gantt-container-section-timeline-header-weeks-year">
+            <b>{year}</b>
+          </div>
+          <div ref={yearsContainerRef} className="gantt-container-section-timeline-header-weeks-content">
+            {weeksByYear[year].map((week, index) => {
               const startOfWeek = moment(week.start, "YYYY-MM-DD");
               const endOfWeek = moment(week.end, "YYYY-MM-DD");
               const today = moment().startOf("day");
               const isCurrentWeek = today.isBetween(
-                  startOfWeek,
-                  endOfWeek.add(2, 'days'),
-                  null,
-                  "[]"
+                startOfWeek,
+                endOfWeek.add(2, 'days'),
+                null,
+                "[]"
               );
               const isWeekHaveTask = weekHaveTask(props.users, startOfWeek, endOfWeek) || isCurrentWeek;
-                // Check if it's the first week of a new year or if the new year starts on a weekend
               const isNewYearWeek =
-                (startOfWeek.isoWeekday() === 1 && startOfWeek.isoWeek() === 1) || // First week of a new year
-                ((startOfWeek.isoWeekday() === 7 || startOfWeek.isoWeekday() === 6) && startOfWeek.isoWeek() === 52) // New year starts on a weekend
+                (startOfWeek.isoWeekday() === 1 && startOfWeek.isoWeek() === 1) ||
+                ((startOfWeek.isoWeekday() === 7 || startOfWeek.isoWeekday() === 6) && startOfWeek.isoWeek() === 52);
 
-                console.log(isNewYearWeek)
-                console.log(startOfWeek.isoWeekday())
-                console.log(startOfWeek.isoWeek())
-
-                return (
-                    <div className="week-container">
-                        {isNewYearWeek && (
-                            <div className="new-year-message">
-                                <b>{endOfWeek.format("YYYY")}</b>
-                            </div>
-                        )}
-                        <div
-                            className={`gantt-container-section-timeline-header-days ${isCurrentWeek ? "today" : ""} ${index === 0 ? "start" : index === timelineWeeks.length - 1 ? "end" : ""} ${isWeekHaveTask ? "" : "no-task"}`}
-                            key={index}
-                            style={props.styleData.daysContainer}>
-                            {isCurrentWeek ? (
-                                <div className="todayDiv">
-                                    <p className="todayText"><b>Aujourd'hui</b></p>
-                                    <p className="weekDate">
-                                        {moment(week.start).format("DD MMMM")} -{" "}
-                                        {moment(week.end).format("DD MMMM")}
-                                    </p>
-                                </div>
-                            ) : (
-                                <p className="weekDate">
-                                    {moment(week.start).format("DD MMMM")} -{" "}
-                                    {moment(week.end).format("DD MMMM")}
-                                </p>
-                            )}
-                        </div>
-                    </div>
-                );
+              return (
+                <div className="week-container" key={index}>
+                  <div
+                    className={`gantt-container-section-timeline-header-days ${isCurrentWeek ? "today" : ""} ${index === 0 ? "start" : index === timelineWeeks.length - 1 ? "end" : ""} ${isWeekHaveTask ? "" : "no-task"}`}
+                    style={props.styleData.daysContainer}
+                  >
+                    {isCurrentWeek ? (
+                      <div className="todayDiv">
+                        <p className="todayText"><b>Aujourd'hui</b></p>
+                        <p className="weekDate">
+                          {moment(week.start).format("DD MMMM")} -{" "}
+                          {moment(week.end).format("DD MMMM")}
+                        </p>
+                      </div>
+                    ) : (
+                      <p className="weekDate">
+                        {moment(week.start).format("DD MMMM")} -{" "}
+                        {moment(week.end).format("DD MMMM")}
+                      </p>
+                    )}
+                  </div>
+                </div>
+              );
             })}
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default GanttTimelineHeader;
