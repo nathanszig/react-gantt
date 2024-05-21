5b00d3dcf1c77c73501f8dcbffbe8fad7288d9cb
Benjamin Burstein <bburstein@steamulo.com>
2024-05-21 17:49:14 +0200

    [RG-24] - Go to the personal view from Teams view

--- src/components/ganttSidebar.jsx
+++ src/components/ganttSidebar.jsx
@@ -1,15 +1,58 @@
 import Icon from '../assets/pictos/arrow-left.svg';
-import {TEAM} from '../assets/utils/ganttUtils';
+import { TEAM } from '../assets/utils/ganttUtils';
 
 const GanttSidebar = (props) => {
   // Déterminer le titre en fonction de la vue
   let cardTitle = props.view === TEAM ? `${props.data.firstName} ${props.data.lastName}` : props.data.name;
 
+  const renderUserTask = (task, index) => {
+    const user = task.user;
+    return (
+      <div
+        className="gantt-container-section-sidebar-dropdown-content-user"
+        key={index}
+      >
+        <div className="gantt-container-section-sidebar-dropdown-content-user-div">
+          <div className="user-info">
+            <img
+              src={user.urlAvatar}
+              alt={`Avatar de ${user.firstName} ${user.lastName}`}
+              className={"avatar-img"}
+            />
+            <div className="user-info-p">
+              <p onClick={() => props.selectUser(user.id)}>{user.firstName} {user.lastName}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderProject = (project, index) => {
+    return (
+      <div
+        className="gantt-container-section-sidebar-dropdown-content-project"
+        key={index}
+      >
+        <div className="gantt-container-section-sidebar-dropdown-content-project-div">
+          <div className="project-info">
+            <div className="project-info-p">
+              <p>{project.name}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="gantt-container-section-sidebar-tasks project" style={props.styleData.sidebarProjects}>
       <div className="gantt-container-section-sidebar-task">
         <div className="gantt-container-section-sidebar-task-client">
-          <p className="gantt-container-section-sidebar-task-client-name">
+          <p
+            className={`gantt-container-section-sidebar-task-client-name${props.view === TEAM ?'-team':''} `}
+            onClick={props.view === TEAM ? () => props.selectUser(props.data.id) : undefined}
+          >
             {cardTitle}
           </p>
         </div>
@@ -30,47 +73,14 @@ const GanttSidebar = (props) => {
       </div>
       {props.data.id === props.selectedDropdownId && (
         <div className="gantt-container-section-sidebar-dropdown-content">
-          {props.view !== TEAM ? props.data.tasks.map((task, index) => {
-            const user = task.user;
-            return (
-              <div
-                className="gantt-container-section-sidebar-dropdown-content-user"
-                key={index}
-              >
-                <div className="gantt-container-section-sidebar-dropdown-content-user-div">
-                  <div className="user-info">
-                    <img
-                      src={user.urlAvatar}
-                      alt={`Avatar de ${user.firstName} ${user.lastName}`}
-                      className={"avatar-img"}
-                    />
-                    <div className="user-info-p">
-                      <p onClick={()=>props.selectUser(user.id)}>{user.firstName} {user.lastName}</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          }) : props.data.projects.map((project, index) => {
-            return (
-              <div
-                className="gantt-container-section-sidebar-dropdown-content-project"
-                key={index}
-              >
-                <div className="gantt-container-section-sidebar-dropdown-content-project-div">
-                  <div className="project-info">
-                    <div className="project-info-p">
-                      <p>{project.name}</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+          {props.view !== TEAM
+            ? props.data.tasks.map(renderUserTask)
+            : props.data.projects.map(renderProject)
+          }
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
 export default GanttSidebar;
