5d77714f912fb8968f7cdf2c682328cabab9b3e5
Martin <mdeboutray@cci-paris-idf.fr>
2024-07-09 14:32:33 +0200

    first draft of the data

---
+++ src/constants/realData.js
@@ -0,0 +1,576 @@
+export const realData = {
+  "users": [
+    {
+      "id": 1,
+      "firstName": "Benjamin",
+      "lastName": "Burstein",
+      "urlAvatar": 'https://imgs.search.brave.com/t4L94ou3SfcKiE_0WyoTVv5QuhOCRaI5DDe29Lg75Oc/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni8xNDA3Mi8xNDA3/MjY1Ni5wbmc',
+      "tasks": [
+        {
+          "id": 3,
+          "project": {
+            "id": 2,
+            "name": "RG-2 Design de la data"
+          },
+          "name": "Structure des données",
+          "start": "02/10/2023",
+          "end": "15/10/2023",
+          "description": "Imaginer la structure des données et les relations entre les différentes entités",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 7,
+          "project": {
+            "id": 4,
+            "name": "RG-4 Création d'une vue projet"
+          },
+          "name": "Création de la vue projet",
+          "start": "01/11/2023",
+          "end": "21/11/2023",
+          "description": "Création de la vue projet pour visualiser les tâches et les dépendances",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 8,
+          "project": {
+            "id": 5,
+            "name": "RG-5 Design des dates"
+          },
+          "name": "Création de la frise",
+          "start": "22/11/2023",
+          "end": "28/11/2023",
+          "description": "Création de la frise pour visualiser les dates des tâches",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 9,
+          "project": {
+            "id": 6,
+            "name": "RG-6 Ajouter un bouton début, fin et aujourd'hui pour l'affichage des dates"
+          },
+          "name": "Ajouter des boutons de navigation",
+          "start": "29/11/2023",
+          "end": "05/12/2023",
+          "description": "Ajouter des boutons début, fin et aujourd'hui pour la navigation",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 12,
+          "project": {
+            "id": 8,
+            "name": "RG-8 Documentation via docusaurus"
+          },
+          "name": "Ajout au projet",
+          "start": "10/01/2024",
+          "end": "11/01/2024",
+          "description": "Ajout de la documentation au projet",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 13,
+          "project": {
+            "id": 8,
+            "name": "RG-8 Documentation via docusaurus"
+          },
+          "name": "Rédaction de la documentation",
+          "start": "11/01/2024",
+          "end": "21/01/2024",
+          "description": "Rédaction de la documentation du projet",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 15,
+          "project": {
+            "id": 9,
+            "name": "RG-9 Intégration du package sur docusaurus"
+          },
+          "name": "Implémentation du package et ajout à la barre de nav",
+          "start": "11/01/2024",
+          "end": "21/01/2024",
+          "description": "Ajouter le gantt sur la page de démo et ajout de la page dans le menu",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 17,
+          "project": {
+            "id": 11,
+            "name": "RG-11 Trie des projets par ordre chronologique"
+          },
+          "name": "Création de la fonction de tri",
+          "start": "01/02/2024",
+          "end": "07/02/2024",
+          "description": "Implémenter un algo de trie des projets par ordre chronologique",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 18,
+          "project": {
+            "id": 11,
+            "name": "RG-11 Trie des projets par ordre chronologique"
+          },
+          "name": "Implémentation du tri dans l'appli",
+          "start": "01/02/2024",
+          "end": "07/02/2024",
+          "description": "Ajouter le tri des projets dans l'application",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 20,
+          "project": {
+            "id": 12,
+            "name": "RG-12 Modifier la data pour avoir une structure adaptée à la vue projet"
+          },
+          "name": "Modification des fonctions pour tenir compte de la nouvelle structure",
+          "start": "27/12/2023",
+          "end": "02/01/2024",
+          "description": "Adapter les fonctions et les vues pour la nouvelle structure de données",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 21,
+          "project": {
+            "id": 13,
+            "name": "RG-13 Ajouter la data en tant que props"
+          },
+          "name": "Ajouter la data en props",
+          "start": "15/02/2024",
+          "end": "20/02/2024",
+          "description": "Ajouter la data en tant que props dans les composants",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 22,
+          "project": {
+            "id": 14,
+            "name": "RG-14 Gestion des dates avec moments"
+          },
+          "name": "Gestion des dates",
+          "start": "10/02/2024",
+          "end": "20/02/2024",
+          "description": "Gestion des dates avec la bibliothèque moment.js",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 26,
+          "project": {
+            "id": 18,
+            "name": "RG-18 REFACTO"
+          },
+          "name": "Refactoring du projet",
+          "start": "10/03/2024",
+          "end": "20/03/2024",
+          "description": "Refactoring du code pour améliorer la lisibilité et la maintenabilité",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 27,
+          "project": {
+            "id": 19,
+            "name": "RG-19 Afficher les images"
+          },
+          "name": "Afficher les images",
+          "start": "01/03/2024",
+          "end": "07/03/2024",
+          "description": "Implémenter l'affichage des images dans les tâches",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 30,
+          "project": {
+            "id": 22,
+            "name": "RG-22 Lier le gantt avec un jira"
+          },
+          "name": "Lier avec Jira",
+          "start": "10/03/2024",
+          "end": "20/03/2024",
+          "description": "Lier le Gantt avec un système Jira pour la gestion des tickets",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 31,
+          "project": {
+            "id": 23,
+            "name": "RG-23 Mode month"
+          },
+          "name": "Ajouter le mode month",
+          "start": "25/03/2024",
+          "end": "31/03/2024",
+          "description": "Ajouter la vue mensuelle au Gantt",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 32,
+          "project": {
+            "id": 24,
+            "name": "RG-24 Vue utilisateurs"
+          },
+          "name": "Créer la vue utilisateurs",
+          "start": "01/04/2024",
+          "end": "10/04/2024",
+          "description": "Créer une vue dédiée aux utilisateurs",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 34,
+          "project": {
+            "id": 26,
+            "name": "RG-26 Ajouter les années sur la timeline"
+          },
+          "name": "Ajouter les années",
+          "start": "15/04/2024",
+          "end": "20/04/2024",
+          "description": "Ajouter les années sur la timeline",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 36,
+          "project": {
+            "id": 28,
+            "name": "RG-28 Add duration of tasks"
+          },
+          "name": "Ajouter la durée des tâches",
+          "start": "25/04/2024",
+          "end": "30/04/2024",
+          "description": "Ajouter la durée des tâches dans le Gantt",
+          "taskImgUrl": "",
+        },
+      ]
+    },
+    {
+      "id": 2,
+      "firstName": "Nathan",
+      "lastName": "Szigeti",
+      "urlAvatar": 'https://imgs.search.brave.com/t4L94ou3SfcKiE_0WyoTVv5QuhOCRaI5DDe29Lg75Oc/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni8xNDA3Mi8xNDA3/MjY1Ni5wbmc',
+      "tasks": [
+        {
+          "id": 1,
+          "project": {
+            "id": 1,
+            "name": "RG-1 Design du Gantt général"
+          },
+          "name": "Fonctionnalités à implémenter",
+          "start": "02/10/2023",
+          "end": "08/10/2023",
+          "description": "Création d'une vision de l'application et des fonctionnalités à implémenter",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 2,
+          "project": {
+            "id": 1,
+            "name": "RG-1 Design du Gantt général"
+          },
+          "name": "Structure de l'application",
+          "start": "09/10/2023",
+          "end": "15/10/2023",
+          "description": "Réflexion sur les composants et la structure de l'application",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 5,
+          "project": {
+            "id": 3,
+            "name": "RG-3 Customisation du style"
+          },
+          "name": "Styles de base",
+          "start": "16/10/2023",
+          "end": "20/10/2023",
+          "description": "Choix des couleurs et des styles de l'application",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 6,
+          "project": {
+            "id": 3,
+            "name": "RG-3 Customisation du style"
+          },
+          "name": "Imagination du système de customisation",
+          "start": "21/10/2023",
+          "end": "28/10/2023",
+          "description": "Imaginer un système de customisation pour les utilisateurs de l'application",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 7,
+          "project": {
+            "id": 4,
+            "name": "RG-4 Création d'une vue projet"
+          },
+          "name": "Création de la vue projet",
+          "start": "01/11/2023",
+          "end": "21/11/2023",
+          "description": "Création de la vue projet pour visualiser les tâches et les dépendances",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 10,
+          "project": {
+            "id": 7,
+            "name": "RG-7 Build et push sur npm"
+          },
+          "name": "Build",
+          "start": "01/02/2024",
+          "end": "10/02/2024",
+          "description": "Build du projet pour l'exporter sur npm",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 11,
+          "project": {
+            "id": 7,
+            "name": "RG-7 Build et push sur npm"
+          },
+          "name": "Push",
+          "start": "01/02/2024",
+          "end": "10/02/2024",
+          "description": "Envoyer la librairie sur npm",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 13,
+          "project": {
+            "id": 8,
+            "name": "RG-8 Documentation via docusaurus"
+          },
+          "name": "Rédaction de la documentation",
+          "start": "11/01/2024",
+          "end": "21/01/2024",
+          "description": "Rédaction de la documentation du projet",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 14,
+          "project": {
+            "id": 9,
+            "name": "RG-9 Intégration du package sur docusaurus"
+          },
+          "name": "Ajout de la page à la doc",
+          "start": "11/01/2024",
+          "end": "21/01/2024",
+          "description": "Création de la page de documentation pour le package",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 16,
+          "project": {
+            "id": 10,
+            "name": "RG-10 Build du projet et jsx"
+          },
+          "name": "Build et JSX",
+          "start": "01/02/2024",
+          "end": "10/02/2024",
+          "description": "Build du projet et intégration du JSX",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 21,
+          "project": {
+            "id": 13,
+            "name": "RG-13 Ajouter la data en tant que props"
+          },
+          "name": "Ajouter la data en props",
+          "start": "15/02/2024",
+          "end": "20/02/2024",
+          "description": "Ajouter la data en tant que props dans les composants",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 23,
+          "project": {
+            "id": 15,
+            "name": "RG-15 Ne pas afficher les weekend"
+          },
+          "name": "Ne pas afficher les weekends",
+          "start": "25/02/2024",
+          "end": "28/02/2024",
+          "description": "Configurer le Gantt pour ne pas afficher les weekends",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 24,
+          "project": {
+            "id": 16,
+            "name": "RG-16 Fix - bug sur l'appartenance d'une date à une semaine"
+          },
+          "name": "Fix bug date-semaine",
+          "start": "01/03/2024",
+          "end": "05/03/2024",
+          "description": "Corriger le bug sur l'appartenance d'une date à une semaine",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 26,
+          "project": {
+            "id": 18,
+            "name": "RG-18 REFACTO"
+          },
+          "name": "Refactoring du projet",
+          "start": "10/03/2024",
+          "end": "20/03/2024",
+          "description": "Refactoring du code pour améliorer la lisibilité et la maintenabilité",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 29,
+          "project": {
+            "id": 21,
+            "name": "RG-21 Fold les semaines vides"
+          },
+          "name": "Plier les semaines vides",
+          "start": "25/03/2024",
+          "end": "31/03/2024",
+          "description": "Configurer le Gantt pour plier les semaines sans tâche",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 33,
+          "project": {
+            "id": 25,
+            "name": "RG-25 Réorganiser les fichiers des composants"
+          },
+          "name": "Réorganisation des fichiers",
+          "start": "01/04/2024",
+          "end": "10/04/2024",
+          "description": "Réorganiser les fichiers des composants pour une meilleure structure",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 35,
+          "project": {
+            "id": 27,
+            "name": "RG-27 Créer plusieurs design par défaut"
+          },
+          "name": "Designs par défaut",
+          "start": "15/04/2024",
+          "end": "25/04/2024",
+          "description": "Créer plusieurs designs par défaut pour le Gantt",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 37,
+          "project": {
+            "id": 29,
+            "name": "RG-29 Refonte graphique"
+          },
+          "name": "Refonte graphique",
+          "start": "01/05/2024",
+          "end": "10/05/2024",
+          "description": "Refonte graphique complète du projet",
+          "taskImgUrl": "",
+        },
+      ]
+    },
+    {
+      "id": 3,
+      "firstName": "Alex",
+      "lastName": "Guillemin",
+      "urlAvatar": 'https://imgs.search.brave.com/t4L94ou3SfcKiE_0WyoTVv5QuhOCRaI5DDe29Lg75Oc/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni8xNDA3Mi8xNDA3/MjY1Ni5wbmc',
+      "tasks": [
+        {
+          "id": 4,
+          "project": {
+            "id": 2,
+            "name": "RG-2 Design de la data"
+          },
+          "name": "Création de données de test",
+          "start": "16/10/2023",
+          "end": "20/10/2023",
+          "description": "Création de données de test pour les premières fonctionnalités de l'application",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 19,
+          "project": {
+            "id": 12,
+            "name": "RG-12 Modifier la data pour avoir une structure adaptée à la vue projet"
+          },
+          "name": "Restrucuration du fichier de données",
+          "start": "27/12/2023",
+          "end": "02/01/2024",
+          "description": "Restructuration du fichier de données pour une meilleure lisibilité",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 26,
+          "project": {
+            "id": 18,
+            "name": "RG-18 REFACTO"
+          },
+          "name": "Refactoring du projet",
+          "start": "10/03/2024",
+          "end": "20/03/2024",
+          "description": "Refactoring du code pour améliorer la lisibilité et la maintenabilité",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 28,
+          "project": {
+            "id": 20,
+            "name": "RG-20 Personnaliser la couleur des cards par projet"
+          },
+          "name": "Personnaliser les couleurs",
+          "start": "01/03/2024",
+          "end": "10/03/2024",
+          "description": "Personnaliser la couleur des cards par projet",
+          "taskImgUrl": "",
+        },
+      ]
+    },
+    {
+      "id": 4,
+      "firstName": "Martin",
+      "lastName": "de Boutray",
+      "urlAvatar": 'https://imgs.search.brave.com/t4L94ou3SfcKiE_0WyoTVv5QuhOCRaI5DDe29Lg75Oc/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni8xNDA3Mi8xNDA3/MjY1Ni5wbmc',
+      "tasks": [
+        {
+          "id": 7,
+          "project": {
+            "id": 4,
+            "name": "RG-4 Création d'une vue projet"
+          },
+          "name": "Création de la vue projet",
+          "start": "01/11/2023",
+          "end": "21/11/2023",
+          "description": "Création de la vue projet pour visualiser les tâches et les dépendances",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 25,
+          "project": {
+            "id": 17,
+            "name": "RG-17 Créer une vue perso"
+          },
+          "name": "Vue personnalisée",
+          "start": "01/06/2024",
+          "end": "15/06/2024",
+          "description": "Créer une vue personnalisée pour les utilisateurs",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 26,
+          "project": {
+            "id": 18,
+            "name": "RG-18 REFACTO"
+          },
+          "name": "Refactoring du projet",
+          "start": "10/03/2024",
+          "end": "20/03/2024",
+          "description": "Refactoring du code pour améliorer la lisibilité et la maintenabilité",
+          "taskImgUrl": "",
+        },
+        {
+          "id": 28,
+          "project": {
+            "id": 20,
+            "name": "RG-20 Personnaliser la couleur des cards par projet"
+          },
+          "name": "Personnaliser les couleurs",
+          "start": "01/03/2024",
+          "end": "10/03/2024",
+          "description": "Personnaliser la couleur des cards par projet",
+          "taskImgUrl": "",
+        },
+      ]
+    },
+  ]
+}
